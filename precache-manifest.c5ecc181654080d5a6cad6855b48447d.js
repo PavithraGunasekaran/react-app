e861ad1bb6aac7752169fa11210fbfc9c982c41a
PavithraGunasekaran <pavithra.g13@gmail.com>
2020-06-16 10:11:14 +0530

    Updates

---
+++ precache-manifest.c5ecc181654080d5a6cad6855b48447d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf7ae2357da6007be41a318c87ea388",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "cc7e7f7da41455cd1299",
+    "url": "/react-app/static/css/main.765ea92c.chunk.css"
+  },
+  {
+    "revision": "af5b99b9878dee53e78a",
+    "url": "/react-app/static/js/2.95e655bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.95e655bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7e7f7da41455cd1299",
+    "url": "/react-app/static/js/main.8f316fc8.chunk.js"
+  },
+  {
+    "revision": "4ff6ad24ca508f9bde4d",
+    "url": "/react-app/static/js/runtime-main.eec1a2cf.js"
+  }
+]);
